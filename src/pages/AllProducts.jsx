274095a9d7779017fefb99e364d8e3c26c537c1e
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-08-16 19:08:31 +0600

    Add some new style on the all products page

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -13,7 +13,9 @@ const AllProducts = () => {
     const [selectedCategory, setSelectedCategory] = useState('')
     const [searchType, setSearchType] = useState('')
     const [minValue, setMinValue] = useState(0);
-    const [maxValue, setMaxValue] = useState(100);
+    const [maxValue, setMaxValue] = useState(0);
+    const [minPrice, setMinPrice] = useState(0)
+    const [maxPrice, setMaxPrice] = useState(100000)
 
     const handleSearch = (e) => {
         e.preventDefault()
@@ -25,17 +27,21 @@ const AllProducts = () => {
         setSelectedBrand('')
         setSelectedCategory('')
         setSearchType('')
+        setMinPrice(0)
+        setMaxValue(0)
+        setMaxPrice(100000)
+        setMinValue(0)
     }
 
 
     useEffect(() => {
-        fetch(`http://localhost:5000/products?name=${search}&brandName=${selectedBrand}&categoryName=${selectedCategory}`)
+        fetch(`http://localhost:5000/products?name=${search}&brandName=${selectedBrand}&categoryName=${selectedCategory}&maxPrice=${maxPrice}&minPrice=${minPrice}`)
             .then(res => res.json())
             .then(data => {
                 setProducts(data)
                 console.log(data)
             })
-    }, [search, selectedBrand, selectedCategory])
+    }, [search, selectedBrand, selectedCategory, maxPrice, minPrice])
 
     useEffect(() => {
         fetch(`http://localhost:5000/unique-values`)
@@ -54,40 +60,15 @@ const AllProducts = () => {
 
 
     const handleSet = () => {
-
+        setMinPrice(minValue)
+        setMaxPrice(maxValue)
     };
 
 
     console.log(uniqueStatus)
 
     return (
-        <div className='container mx-auto'>
-            {/* search box div */}
-            <div className='p-4 max-w-[300px] mx-auto'>
-                <label className="input input-bordered flex items-center gap-2">
-                    <form onSubmit={handleSearch}>
-                        <input
-                            type="text"
-                            name='searchValue'
-                            className="grow"
-                            placeholder="Search"
-                            value={searchType}
-                            onChange={(e) => setSearchType(e.target.value)}
-                        />
-                        <button type='submit'></button>
-                    </form>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-5 w-5 opacity-80">
-                        <path
-                            fillRule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clipRule="evenodd" />
-                    </svg>
-                </label>
-            </div>
+        <div className='container mx-auto py-10'>
 
             {/* product area div */}
 
@@ -97,17 +78,46 @@ const AllProducts = () => {
                     no products found
                 </div>
             }
+
             <div className='flex flex-col lg:flex-row gap-10'>
-                <div className='lg:w-1/5 '>
+                {/* Categorization item div */}
+                <div className='lg:w-1/5 space-y-6'>
+                    {/* search box div */}
+                    <div className='py-4 max-w-[300px] '>
+                        <label className="input input-bordered flex items-center gap-2">
+                            <form onSubmit={handleSearch}>
+                                <input
+                                    type="text"
+                                    name='searchValue'
+                                    className="grow"
+                                    placeholder="Search"
+                                    value={searchType}
+                                    onChange={(e) => setSearchType(e.target.value)}
+                                />
+                                <button type='submit'></button>
+                            </form>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                className="h-5 w-5 opacity-80">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                    clipRule="evenodd" />
+                            </svg>
+                        </label>
+                    </div>
                     {/* price range customization div */}
                     <div>
-                        <h1>Price Range</h1>
-                        <div className=" flex flex-col justify-center items-center space-y-4">
+                        <h1 className='text-[16px]  font-medium text-start text-gray-500'>Price Range</h1>
+                        <div className=" flex flex-col justify-center items-start space-y-4">
                             <div className="flex space-x-4">
                                 <div>
                                     <input
                                         type="number"
                                         value={minValue}
+                                        name='minValue'
                                         onChange={(e) => setMinValue(parseInt(e.target.value))}
                                         className="px-4 max-w-[90px] py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                                         min={0}
@@ -117,6 +127,7 @@ const AllProducts = () => {
                                     <input
                                         type="number"
                                         value={maxValue}
+                                        name='maxValue'
                                         onChange={(e) => setMaxValue(parseInt(e.target.value))}
                                         className="px-4 max-w-[90px] py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                                         min={minValue}
@@ -133,7 +144,7 @@ const AllProducts = () => {
                     </div>
                     {/* product name customization div */}
                     <div>
-                        <h1>Brand Name</h1>
+                        <h1 className='text-[16px]  font-medium text-start text-gray-500'>Brand Name</h1>
                         <div className='flex border overflow-y-auto flex-col gap-2 max-h-[140px] p-4'>
                             {
                                 uniqueStatus?.uniqueBrands?.map((brand, index) => (
@@ -157,7 +168,7 @@ const AllProducts = () => {
                         </div>
                     </div>
                     <div>
-                        <h1>Category</h1>
+                        <h1 className='text-[16px]  font-medium text-start text-gray-500'>Category</h1>
                         <div className='flex border overflow-y-auto flex-col gap-2 max-h-[140px] p-4'>
                             {
                                 uniqueStatus?.uniqueCategories?.map((category, index) => (
@@ -180,14 +191,14 @@ const AllProducts = () => {
                             }
                         </div>
                     </div>
-                    <button onClick={handleReset} className='px-4 py-2 bg-green-400 text-white font-bold'>Reset</button>
+                    <button onClick={handleReset} className='px-4 py-2 bg-blue-500 rounded-md text-white font-bold'>Reset</button>
 
                 </div>
                 <div className='grid lg:w-4/5 grid-cols-1 mx-auto md:grid-cols-3 lg:grid-cols-4 gap-4'>
                     {
                         // single product card
-                        products?.map((product) => <div className='productCard relative border rounded-md hover:shadow-md space-y-2 p-4 max-w-[300px] mx-auto' key={product._id}>
-                            <img className='productImage h-[300px] w-[300px] object-cover object-center' src={product.productImage} alt={product.productName} />
+                        products?.map((product) => <div className='productCard max-h-[450px] relative border rounded-md hover:shadow-md space-y-2 p-4 max-w-[300px] mx-auto' key={product._id}>
+                            <img className='productImage h-[200px] w-[200px] object-cover object-center' src={product.productImage} alt={product.productName} />
                             <p className='absolute bg-green-500 rounded-full px-2 flex gap-1 items-center top-4 right-6 text-white'><FaStar className='text-yellow-300' />{product?.ratings}</p>
                             <h2 className='font-bold text-gray-500'>{product?.productName}</h2>
                             <p className='text-green-500 font-semibold'>${product?.price}</p>
--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-cyan-500 bg-opacity-75 rounded-full m-0 px-4">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
